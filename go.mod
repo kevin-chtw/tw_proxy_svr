dd57920096fb044662c0fac6c1271cd5f098d887
kevin-chtw <451151014@qq.com>
2025-07-31 16:52:46 +0800

    设置nats 和etcd的地址

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/kevin-chtw/tw_proxy_svr
 go 1.24.0
 
 require (
-	github.com/kevin-chtw/tw_proto v0.0.0-20250616100835-700e15a73cc2
 	github.com/sirupsen/logrus v1.9.3
 	github.com/topfreegames/pitaya/v3 v3.0.0-beta.6
 )
--- main.go
+++ main.go
@@ -20,6 +20,10 @@ func main() {
 	logrus.SetLevel(logrus.DebugLevel)
 
 	config := config.NewDefaultPitayaConfig()
+	config.Cluster.RPC.Client.Nats.Connect = "nats://192.168.182.128:4222"
+	config.Cluster.RPC.Server.Nats.Connect = "nats://192.168.182.128:4222"
+	config.Cluster.SD.Etcd.Endpoints = []string{"http://192.168.182.128:2379"}
+	config.Groups.Etcd.Endpoints = []string{"http://192.168.182.128:2379"}
 	builder := pitaya.NewDefaultBuilder(true, serverType, pitaya.Cluster, map[string]string{}, *config)
 	builder.AddAcceptor(acceptor.NewTCPAcceptor(fmt.Sprintf(":%d", *port)))
 	app = builder.Build()
