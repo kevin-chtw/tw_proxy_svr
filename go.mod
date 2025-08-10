dee7742694dd373fb35cb79962bdfba251121864
chentw <451151014@qq.com>
2025-08-10 21:30:32 +0800

    session

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/kevin-chtw/tw_proxy_svr
 go 1.24.0
 
 require (
+	github.com/kevin-chtw/tw_proto v0.0.0-20250809141037-7a0771bd5f37
 	github.com/sirupsen/logrus v1.9.3
 	github.com/topfreegames/pitaya/v3 v3.0.0-beta.6
 )
+
+replace github.com/kevin-chtw/tw_proto  => ../tw_proto
 
 require (
 	github.com/DataDog/datadog-go v4.8.3+incompatible // indirect
--- main.go
+++ main.go
@@ -1,16 +1,67 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 
+	"github.com/kevin-chtw/tw_proto/sproto"
 	"github.com/sirupsen/logrus"
 	pitaya "github.com/topfreegames/pitaya/v3/pkg"
 	"github.com/topfreegames/pitaya/v3/pkg/acceptor"
 	"github.com/topfreegames/pitaya/v3/pkg/config"
+	"github.com/topfreegames/pitaya/v3/pkg/modules"
+	"github.com/topfreegames/pitaya/v3/pkg/session"
 )
 
 var app pitaya.Pitaya
+
+func OnSessionClose(s session.Session) {
+	uid := s.UID()
+	logrus.Infof("session closed: %s", uid)
+	module, err := app.GetModule("matchingstorage")
+	if err != nil {
+		return
+	}
+	ms := module.(*modules.ETCDBindingStorage)
+	serverId, err := ms.GetUserFrontendID(uid, "match")
+	if err != nil || serverId == "" {
+		return
+	}
+
+	req := &sproto.Proxy2MatchReq{
+		Req: &sproto.Proxy2MatchReq_OfflineReq{
+			OfflineReq: &sproto.OfflineReq{
+				Uid: uid,
+			},
+		},
+	}
+	app.RPCTo(context.Background(), serverId, "match.player.offline", nil, req)
+}
+
+func OnAfterSessionBind(ctx context.Context, s session.Session) error {
+	uid := s.UID()
+	logrus.Infof("session binded: %s", uid)
+	module, err := app.GetModule("matchingstorage")
+	if err != nil {
+		return nil
+	}
+	ms := module.(*modules.ETCDBindingStorage)
+	serverId, err := ms.GetUserFrontendID(uid, "match")
+	if err != nil || serverId == "" {
+		return nil
+	}
+
+	req := &sproto.Proxy2MatchReq{
+		Req: &sproto.Proxy2MatchReq_OnlineReq{
+			OnlineReq: &sproto.OnlineReq{
+				Uid: uid,
+			},
+		},
+	}
+	app.RPCTo(context.Background(), serverId, "match.player.online", nil, req)
+	return nil
+}
 
 func main() {
 	serverType := "proxy"
@@ -20,15 +71,18 @@ func main() {
 	logrus.SetLevel(logrus.DebugLevel)
 
 	config := config.NewDefaultPitayaConfig()
-	config.Cluster.RPC.Client.Nats.Connect = "nats://192.168.182.128:4222"
-	config.Cluster.RPC.Server.Nats.Connect = "nats://192.168.182.128:4222"
-	config.Cluster.SD.Etcd.Endpoints = []string{"http://192.168.182.128:2379"}
-	config.Groups.Etcd.Endpoints = []string{"http://192.168.182.128:2379"}
+	// config.Cluster.RPC.Client.Nats.Connect = "nats://192.168.182.128:4222"
+	// config.Cluster.RPC.Server.Nats.Connect = "nats://192.168.182.128:4222"
+	// config.Cluster.SD.Etcd.Endpoints = []string{"http://192.168.182.128:2379"}
+	// config.Groups.Etcd.Endpoints = []string{"http://192.168.182.128:2379"}
 	builder := pitaya.NewDefaultBuilder(true, serverType, pitaya.Cluster, map[string]string{}, *config)
 	builder.AddAcceptor(acceptor.NewTCPAcceptor(fmt.Sprintf(":%d", *port)))
+	builder.SessionPool.OnSessionClose(OnSessionClose)
+	builder.SessionPool.OnAfterSessionBind(OnAfterSessionBind)
 	app = builder.Build()
-
 	defer app.Shutdown()
+	bs := modules.NewETCDBindingStorage(builder.Server, builder.SessionPool, builder.Config.Modules.BindingStorage.Etcd)
+	app.RegisterModule(bs, "matchingstorage")
 
 	app.Start()
 }
