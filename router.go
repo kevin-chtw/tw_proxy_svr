153dc38ae7b6d5727174b1bc0116a903704faffd
chentw <451151014@qq.com>
2025-10-26 16:24:16 +0800

    解析

--- router.go
+++ router.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"math/rand"
 
 	"github.com/kevin-chtw/tw_proto/cproto"
 	"github.com/topfreegames/pitaya/v3/pkg/cluster"
 	"github.com/topfreegames/pitaya/v3/pkg/route"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 // 自定义 Router
 func GameRouter(ctx context.Context, route *route.Route, payload []byte, servers map[string]*cluster.Server) (*cluster.Server, error) {
 	req := &cproto.GameReq{}
-	if err := json.Unmarshal(payload, &req); err == nil && req.Serverid != "" {
+	if err := protojson.Unmarshal(payload, req); err == nil && req.Serverid != "" {
 		if svr, ok := servers[req.Serverid]; ok {
 			return svr, nil
 		}
